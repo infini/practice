595fe535846937015a8c492496329749d2e96886
ChangSeop.Kim <infini2753@gmail.com>
2013-07-05 18:42:57 +0900

    t

--- msgpack/main.cpp
+++ msgpack/main.cpp
@@ -15,28 +15,39 @@
 
 int _tmain()
 {
+	std::vector<int> containerInt;
+	for( int nth = 0; nth < 1000; ++nth ) {
+		containerInt.push_back( nth );
+	}
+
 	std::vector<std::string> target;
 	target.push_back("Hello,");
 	target.push_back("World!");
 
 	// Serialize it.
 	msgpack::sbuffer buffer;  // simple buffer
-	msgpack::pack(&buffer, target);
+	msgpack::pack(&buffer, containerInt);
+
+	std::cout << "packed buffer size : " << buffer.size() << std::endl;
 
 	// Deserialize the serialized data.
  	msgpack::unpacked msg;    // includes memory pool and deserialized object
  	msgpack::unpack(&msg, buffer.data(), buffer.size());
  	msgpack::object obj = msg.get();
+
+	std::cout << "unpacked msg size : " << sizeof( msg ) << std::endl;
  
  	// Print the deserialized object to stdout.
-	std::cout << obj << std::endl;    // ["Hello," "World!"]
+	std::cout << "msg obj : " << obj << std::endl;    // ["Hello," "World!"]
  
  	// Convert the deserialized object to staticaly typed object.
- 	std::vector<std::string> result;
- 	obj.convert(&result);
- 
- 	// If the type is mismatched, it throws msgpack::type_error.
- 	obj.as<int>();
+  	std::vector<int> result;
+  	obj.convert(&result);
+  
+  	// If the type is mismatched, it throws msgpack::type_error.
+  	std::vector<int> temp = obj.as< std::vector<int> >();
+
+	std::cout << temp.size() << std::endl;
 
 	return 0;
 }
